9b6224694a38d03a153d6da31499e67e7bfa1f4d
downpool <wanpile@g.cnu.ac.kr>
2021-12-19 18:54:56 +0900

    12/19

--- PS_js/baekjoon/15988.js
+++ PS_js/baekjoon/15988.js
@@ -31,3 +31,23 @@ function solution(n) {
 for(let i = 0; i < inp.length; i++){
     console.log(solution(inp[i]));
 }
+
+//sol2 clear
+let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+for(let i = 0; i < input.length; i++){
+    input[i] = Number(input[i]);
+}
+let max = Math.max(...input);
+let memo = Array(max + 1).fill(0);
+memo[0] = 0;
+memo[1] = 1;
+memo[2] = 2;
+memo[3] = 4;
+memo[4] = 7;
+for(let i = 5; i <= max; i++){
+    memo[i] = memo[i - 1] + memo[i - 2] + memo[i - 3];
+    memo[i] %= 1000000009;
+}
+for(let i = 1; i < input.length; i++){
+    console.log(memo[input[i]]);
+}
