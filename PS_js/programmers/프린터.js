ddaabca23bd858c7ec2ca0dcce7cf4c4fc6c603c
donghyeon choi <wanpile@g.cnu.ac.kr>
2022-02-25 15:23:09 +0900

    2/25

---
+++ PS_js/programmers/프린터.js
@@ -0,0 +1,21 @@
+function solution(priorities, location) {
+    let answer = 0;
+    let arr = priorities.slice(0);
+    let ret = arr.map((_, i) => i);
+    let ansarr = [];
+    while(arr.length){
+        let max = Math.max(...arr);
+        while(arr[0] != max){
+            let node = arr.shift();
+            let pos = ret.shift();
+            arr.push(node);
+            ret.push(pos);
+        }
+        arr.shift();
+        let pos = ret.shift();
+        ansarr.push(pos);
+    }
+    //??
+    answer = ansarr.indexOf(location) + 1;
+    return answer;
+}
