1ca7a152a8084fcefdf466dffab33f03e6c4eb4e
donghyeon choi <wanpile@g.cnu.ac.kr>
2022-04-23 19:27:42 +0900

    템플릿

---
+++ PS_java/for(재혁)/3/five.java
@@ -0,0 +1,7 @@
+import java.util.*;
+
+public class five {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ PS_java/for(재혁)/4/two.java
@@ -0,0 +1,7 @@
+import java.util.*;
+
+public class two {
+    public static void main(String[] args) {
+        
+    }
+}
