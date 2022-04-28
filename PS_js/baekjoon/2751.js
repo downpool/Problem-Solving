e6c7949dd6960b0e719cac715a9d4a655e71ce36
donghyeon choi <wanpile@g.cnu.ac.kr>
2022-04-28 13:49:46 +0900

    4/28

--- PS_js/baekjoon/2751.js
+++ PS_js/baekjoon/2751.js
@@ -1,39 +1,51 @@
-let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
-input.shift();
-const result = quickSortStarter(input).join('\n');
+// let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+// input.shift();
+// const result = quickSortStarter(input).join('\n');
 
-function quickSortStarter(arr) {
-  if (!arr.length) {
-    return arr;
-  }
-  for(let i = 0; i < arr.length; i++){
-    arr[i] = Number(arr[i]);
-  }
-  return quickSort(arr, 0, arr.length - 1);
-}
+// function quickSortStarter(arr) {
+//   if (!arr.length) {
+//     return arr;
+//   }
+//   for(let i = 0; i < arr.length; i++){
+//     arr[i] = Number(arr[i]);
+//   }
+//   return quickSort(arr, 0, arr.length - 1);
+// }
 
-function quickSort(array, l, r) {
-  const pivot = array[Math.floor((l + r) / 2)];
-  let left = l;
-  let right = r;
+// function quickSort(array, l, r) {
+//   const pivot = array[Math.floor((l + r) / 2)];
+//   let left = l;
+//   let right = r;
 
-  while (left <= right) {
-    while (array[left] < pivot) left++;
-    while (array[right] > pivot) right--;
+//   while (left <= right) {
+//     while (array[left] < pivot) left++;
+//     while (array[right] > pivot) right--;
 
-    if (left <= right) {
-      const temp = array[left];
-      array[left] = array[right];
-      array[right] = temp;
-      left++;
-      right--;
-    }
-  }
+//     if (left <= right) {
+//       const temp = array[left];
+//       array[left] = array[right];
+//       array[right] = temp;
+//       left++;
+//       right--;
+//     }
+//   }
 
-  if (l < right) quickSort(array, l, right);
-  if (r > left) quickSort(array, left, r);
+//   if (l < right) quickSort(array, l, right);
+//   if (r > left) quickSort(array, left, r);
 
-  return array;
-}
+//   return array;
+// }
 
-console.log(result);
+// console.log(result);
+let input = [];
+
+require('readline')
+  .createInterface(process.stdin, process.stdout)
+  .on('line', function (line) {
+    input.push(line.trim());
+  })
+  .on('close', function () {
+    const [N, ...numList] = input.map((e) => Number(e));
+    const result = numList.sort((a, b) => a - b);
+    console.log(result.join('\n'));
+  });
