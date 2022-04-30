ea779a6e316afabdc3f13a91be147bb7afa9220a
donghyeon choi <wanpile@g.cnu.ac.kr>
2022-04-30 10:23:55 +0900

    c++ 템플릿

--- PS_C++/ps/ps/0.cpp
+++ PS_C++/ps/ps/0.cpp
@@ -1,8 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define MIN (x < y ? x : y)
+#define MAX (x > y ? x : y)
+
+
+void solve(){
+
+}
+
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
 
+    solve()
 }
