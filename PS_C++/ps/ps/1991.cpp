3540578ac72f6df73b1b1f91188bfb6f54d50fdc
donghyeon choi <wanpile@g.cnu.ac.kr>
2022-05-01 17:34:42 +0900

    5/1

--- PS_C++/ps/ps/1991.cpp
+++ PS_C++/ps/ps/1991.cpp
@@ -51,3 +51,68 @@ int main(){
     
     return 0;
 }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// struct Node {
+//     int left;
+//     int right;
+// };
+
+// Node a[100];
+
+// void preorder(int x){
+//     if(x == -1) return;
+//     cout << (char)(x + 'A');
+//     preorder(a[x].left);
+//     preorder(a[x].right);
+// }
+
+// void inorder(int x){
+// if(x == -1) return;
+//     inorder(a[x].left);
+//     cout << (char)(x + 'A');
+//     inorder(a[x].right);
+// }
+
+// void lastorder(int x){
+//     if(x == -1) return;
+//     lastorder(a[x].left);
+//     lastorder(a[x].right);
+//     cout << (char)(x + 'A');
+// }
+
+// void solve(){
+//     int n;
+//     cin >> n;
+//     for(int i = 1; i <= n; i++){
+//         char x, y, z;
+//         cin >> x >> y >> z;
+//         x = x - 'A';
+//         if(y == '.'){
+//             a[x].left = -1;
+//         }else{
+//             a[x].left = y - 'A';
+//         }
+//         if(z == '.'){
+//             a[x].right = -1;
+//         }else{
+//             a[x].right = z - 'A';
+//         }
+//     }
+
+//     preorder(0);
+//     cout << "\n";
+//     inorder(0);
+//     cout << "\n";
+//     lastorder(0);
+
+// }
+
+// int main(){
+//     ios::sync_with_stdio(0);
+//     cin.tie(0);
+
+//     solve();
+// }
