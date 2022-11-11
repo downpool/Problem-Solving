7a277f76da52695f933262f2cbbe390b89b101ca
donghyeon choi <wanpile@g.cnu.ac.kr>
2022-11-11 16:55:08 +0900

    11/11

---
+++ PS_js/programmers/intro/유한소수 판별하기.js
@@ -0,0 +1,14 @@
+function solution(a, b) {
+    function gcd(x, y){
+        if(y == 0){
+            return x;
+        }else{
+            return gcd(y, x % y);
+        }
+    }
+
+    let t = b / gcd(a, b);
+    while(t % 2 == 0) t /= 2;
+    while(t % 5 == 0) t /= 5;
+    return (t == 1) ? 1 : 2;
+}
