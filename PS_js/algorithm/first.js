38f82d98723d0fdb17b2addd62456af4212e887d
downpool <downpool.choi@gmail.com>
2023-03-29 11:08:02 +0900

    make pseudocode bfs adj list

--- PS_js/algorithm/first.js
+++ PS_js/algorithm/first.js
@@ -1,8 +1,44 @@
-function BFS(){
+function BFS(graph, n, visited){
+/*
+Adjacency List
+    
+make queue q include n
+
+PRINT n
+
+WHILE q's length is not zero:
+    new value node is q.front
+    q pop front
+
+    FOR e in graph[node]:
+        IF e is not visited:
+            PRINT e
+            q push e
+            visited[e] change true
+
+
+*/
+
+/*
+Adjacency Matrix
+
+
+
+*/
 
 }
 
-function DFS(){
+function DFS(graph, n, visited){
+/*
+Adjacency List
+
+
+*/
+
+/*
+Adjacency Matrix
+
+*/
 
 }
 
