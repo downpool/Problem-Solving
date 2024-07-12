9ecc3103eb4475d5e9af50f28e5ab8fca3ca40d4
donghyeon choi <downpool.choi@gmail.com>
2024-07-12 15:57:43 +0900

    solv

--- PS.swift/2667.swift
+++ PS.swift/2667.swift
@@ -1,16 +1,47 @@
 import Foundation
 
-var grid: [[Int]]
+let n = Int(readLine()!)!
 
-func dfs(i: Int, j: Int, maxi: Int, maxj: Int) {
-    if i < 0 || i >= maxi || j < 0 || j >= maxj || grid[i][j] != "1" {
-        return
+var graph = [[Int]](repeating: [Int](repeating: 0, count: n), count: n)
+
+for i in 0 ..< n {
+    let line = readLine()!.map{Int(String($0))!}
+    for j in 0 ..< n {
+        graph[i][j] = line[j]
+    }
+}
+
+
+
+
+var count = 0
+func dfs(_ x: Int, _ y: Int){
+    if x < 0 || x >= n || y < 0 || y >= n || graph[x][y] == 0 {
+        return 
     }
 
-    grid[i][j] = "0"
+    graph[x][y] = 0
+    count += 1
 
-    dfs(i: i + 1, j: j, maxi: maxi, maxj: maxj)
-    dfs(i: i, j: j + 1, maxi: maxi, maxj: maxj)
-    dfs(i: i, j: j - 1, maxi: maxi, maxj: maxj)
-    dfs(i: i - 1, j: j, maxi: maxi, maxj: maxj)
+    dfs(x, y + 1)
+    dfs(x, y - 1)
+    dfs(x - 1, y)
+    dfs(x + 1, y)
+}
+var cnt = 0
+var add: [Int] = []
+for i in 0 ..< n {
+    for j in 0 ..< n {
+        if graph[i][j] == 1 {
+            dfs(i, j)
+            add.append(count)
+            count = 0
+            cnt += 1
+        }
+    }
+}
+add.sort()
+print(cnt)
+for i in add {
+    print(i)
 }
