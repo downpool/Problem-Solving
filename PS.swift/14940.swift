6609183746ed11a62a1c28440862106b08b1776c
donghyeon choi <downpool.choi@gmail.com>
2024-11-27 08:06:45 +0900

    11/27

---
+++ PS.swift/14940.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+var nm = readLine()!.split(separator: " ").map{Int($0)!}
+var (n, m) = (nm[0], nm[1])
+
+var stack = [Int]()
+
+func dfs(_ start: Int) {
+    if stack.count == m {
+        print(stack.map{String($0)}.joined(separator: " "))
+        return
+    }
+
+    for i in start ..< n + 1 {
+        if !stack.contains(i) {
+            stack.append(i)
+            dfs(i + 1)
+            stack.removeLast()
+        }
+    }
+}
+
+dfs(1)
