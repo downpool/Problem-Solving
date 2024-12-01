f81790c3e6084b25816180ff71831fb189a0408a
donghyeon choi <downpool.choi@gmail.com>
2024-12-01 16:04:33 +0900

    12/1

--- PS.swift/15649.swift
+++ PS.swift/15649.swift
@@ -1,23 +1,24 @@
 import Foundation
 
-let input = readLine()!.split(separator: " ").map { Int($0)! }
-let (n, m) = (input[0], input[1])
-var visited = [Bool](repeating: false, count: n + 1)
+var nm = readLine()!.split(separator: " ").map{Int($0)!}
+var (n, m) = (nm[0], nm[1])
 
-func dfs(_ nums: [Int]) {
-    if nums.count == m {
-        nums.forEach { print($0, terminator: " ") }
-        print()
+var stack = [Int]()
+
+func dfs(){
+    if stack.count == m {
+        print(stack.map{ String($0) }.joined(separator:" "))
         return
     }
-    
+
     for i in 1...n {
-        if !visited[i] {
-            visited[i] = true
-            dfs(nums: nums + [i])
-            visited[i] = false
+        if !stack.contains(i) {
+            stack.append(i)
+            dfs()
+            stack.removeLast()
         }
+
     }
 }
 
-dfs([])
+dfs()
